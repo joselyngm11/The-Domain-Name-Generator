a2018be4344e80f40e1b57ca75a64376ba406850
joselyngm11 <joselyngm.47@gmail.com>
2022-08-24 21:14:00 +0000

    parte basica del proyecto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let result, i, j, k;
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+for (i = 0; i < pronoun.length; i++) {
+  for (j = 0; j < adj.length; j++) {
+    for (k = 0; k < noun.length; k++) {
+      result = pronoun[i] + adj[j] + noun[k] + ".com";
+      console.log(result);
+    }
+  }
+}
