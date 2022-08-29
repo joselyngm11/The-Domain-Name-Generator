cb7b9abe3b7e48c96bae72660ea536486438bd6b
joselyngm11 <joselyngm.47@gmail.com>
2022-08-29 21:22:49 +0000

    Extra added

--- src/app.js
+++ src/app.js
@@ -5,16 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let result, i, j, k;
-var pronoun = ["the", "our"];
-var adj = ["great", "big"];
-var noun = ["jogger", "racoon"];
+let result, i, j, k, m;
+let pronoun = ["the", "our", "nosotros"];
+let adj = ["great", "big", "muy", "menos"];
+let noun = ["jogger", "racoon", "suave", "sabroso", "palabres"];
+let extensions = ["es", "eu", "co", "ve"];
 
 for (i = 0; i < pronoun.length; i++) {
   for (j = 0; j < adj.length; j++) {
     for (k = 0; k < noun.length; k++) {
-      result = pronoun[i] + adj[j] + noun[k] + ".com";
-      console.log(result);
+      for (m = 0; m < extensions.length; m++) {
+        if (noun[k].endsWith(extensions[m])) {
+          console.log(
+            pronoun[i] +
+              adj[j] +
+              noun[k].slice(0, -1 * extensions[m].length) +
+              "." +
+              extensions[m]
+          );
+        } else {
+          console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        }
+      }
     }
   }
 }
